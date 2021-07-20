a51b692acdbe38ab2fbcf222354ec4fc47ea3672
steven-boston <steven.cole.boston@gmail.com>
2021-07-19 19:50:21 -0700

    challenge 6 methods and tests implemented

---
+++ c-sharp/data-structures/DataStructures/CodeChallenges/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using DataStructures;
+
+namespace CodeChallenges
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
