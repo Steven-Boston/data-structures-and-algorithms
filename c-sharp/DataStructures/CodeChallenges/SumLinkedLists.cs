2486e6567711d24ad5c5ddff4c68dd74a6690bb3
Steven-Boston <79598445+Steven-Boston@users.noreply.github.com>
2021-11-08 23:48:53 -0800

    added LinkedSum, tests in progress

---
+++ c-sharp/DataStructures/CodeChallenges/SumLinkedLists.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+namespace CodeChallenges
+{
+  public class SumLinkedLists
+  {
+    public static DataStructures.LinkedList<int> SumLists(DataStructures.LinkedList<int> listA, DataStructures.LinkedList<int> listB)
+    {
+      DataStructures.LinkedList<int> result = new();
+      int add = 0;
+      Node<int> current1 = listA.Head;
+      Node<int> current2 = listB.Head;
+
+      while(current1 != null && current2 != null)
+      {
+        int sum = current1.Value + current2.Value + add;
+        current1 = current1.Next;
+        current2 = current2.Next;
+        if(sum-10<1)
+        {
+          add = 1;
+          result.AppendLinked(sum - 10);
+        }
+        else
+        {
+          add = 0;
+          result.AppendLinked(sum);
+        }
+      }
+      while(current1 != null)
+      {
+        result.AppendLinked(current1.Value+add);
+        add = 0;
+        current1 = current1.Next;
+      }
+      while (current2 != null)
+      {
+        result.AppendLinked(current2.Value + add);
+        add = 0;
+        current2 = current2.Next;
+      }
+      return result;
+    }
+  }
+}
---
+++ c-sharp/DataStructures/CodeChallengesTests/LinkedSumTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+using CodeChallenges;
+using Xunit;
+
+namespace CodeChallengesTests
+{
+  public class LinkedSumTests
+  {
+    [Fact]
+    public void CanAddLists()
+    {
+      DataStructures.LinkedList<int> testList = new();
+
+    }
+  }
+}
